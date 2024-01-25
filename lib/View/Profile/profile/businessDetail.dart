ffbf1cd9a9ef158fcbd891e1330a92e0648570b8
namanalphawizz <naman.palphawizz@gmail.com>
2024-01-25 17:27:20 +0530

    bd

--- lib/View/Profile/profile/businessDetail.dart
+++ lib/View/Profile/profile/businessDetail.dart
@@ -46,165 +46,163 @@ class _EditBusinessDetailScreenState extends State<EditBusinessDetailScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: CommanAppbar(appbarTitle: "Business Detail"),
-      body: Expanded(
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: emailCtrl,
-                    inputFormatters: [RegexFormatter.email],
-                    textInputAction: TextInputAction.next,
-                    decoration: (const InputDecoration())
-                        .applyDefaults(Theme.of(context).inputDecorationTheme)
-                        .copyWith(labelText: "Email ID"),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Enter Email ID";
-                      } else if (!validator.email(value)) {
-                        return "Please enter a valid email";
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: emailCtrl,
+                  inputFormatters: [RegexFormatter.email],
+                  textInputAction: TextInputAction.next,
+                  decoration: (const InputDecoration())
+                      .applyDefaults(Theme.of(context).inputDecorationTheme)
+                      .copyWith(labelText: "Email ID"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Enter Email ID";
+                    } else if (!validator.email(value)) {
+                      return "Please enter a valid email";
+                    }
+                    return null;
+                  },
+                ),
+                const Divider(color: Colors.transparent),
+                TextFormField(
+                  controller: typeCtrl,
+                  textInputAction: TextInputAction.next,
+                  decoration: (const InputDecoration())
+                      .applyDefaults(Theme.of(context).inputDecorationTheme)
+                      .copyWith(labelText: "Business Type*"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Enter businesss type";
+                    }
+                    return null;
+                  },
+                ),
+                const Divider(color: Colors.transparent),
+                TextFormField(
+                  controller: nameCtrl,
+                  textInputAction: TextInputAction.next,
+                  decoration: (const InputDecoration())
+                      .applyDefaults(Theme.of(context).inputDecorationTheme)
+                      .copyWith(labelText: "Company or Business Name*"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Enter Company or Business Name*";
+                    }
+                    return null;
+                  },
+                ),
+                const Divider(color: Colors.transparent),
+                TextFormField(
+                  controller: gstinCtrl,
+                  inputFormatters: [RegexFormatter.regNo],
+                  textCapitalization: TextCapitalization.characters,
+                  maxLength: 15,
+                  textInputAction: TextInputAction.next,
+                  decoration: (const InputDecoration())
+                      .applyDefaults(Theme.of(context).inputDecorationTheme)
+                      .copyWith(labelText: "Enter GSTIN*", counterText: ""),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter GSTIN";
+                    }
+                    if (value.length < 15) {
+                      return "Please enter valid GST";
+                    }
+                    return null;
+                  },
+                ),
+                const Divider(color: Colors.transparent),
+                TextFormField(
+                  controller: regCtrl,
+                  textInputAction: TextInputAction.next,
+                  inputFormatters: [RegexFormatter.regNo],
+                  textCapitalization: TextCapitalization.characters,
+                  maxLength: 21,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter Registration No.";
+                    }
+                    if (value.length < 21) {
+                      return "Please enter valid Reg No.";
+                    }
+                    return null;
+                  },
+                  decoration: (const InputDecoration())
+                      .applyDefaults(Theme.of(context).inputDecorationTheme)
+                      .copyWith(
+                          labelText:
+                              "Business Registration Number (if applicable)",
+                          counterText: ""),
+                ),
+                const Divider(color: Colors.transparent),
+                TextFormField(
+                  controller: tinCtrl,
+                  textInputAction: TextInputAction.done,
+                  inputFormatters: [RegexFormatter.regNo],
+                  textCapitalization: TextCapitalization.characters,
+                  maxLength: 10,
+                  decoration: (const InputDecoration())
+                      .applyDefaults(Theme.of(context).inputDecorationTheme)
+                      .copyWith(
+                          labelText: "Permanent Account Number (PAN)*",
+                          counterText: ""),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter Permanent Account Number";
+                    }
+                    if (value.length < 10) {
+                      return "Please enter Permanent Account Number";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: height * .24,
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        await profilePro
+                            .updateBusinessDetail(
+                              bussiness_email_id: emailCtrl.text.toString(),
+                              bussiness_type: typeCtrl.text.toString(),
+                              company_name: nameCtrl.text.toString(),
+                              gst_in: gstinCtrl.text.toString().toUpperCase(),
+                              bussiness_registeration_number:
+                                  regCtrl.text.toString().toUpperCase(),
+                              tax_identification_number:
+                                  tinCtrl.text.toString().toUpperCase(),
+                            )
+                            .then(
+                              (value) => Fluttertoast.showToast(
+                                msg: value['msg'],
+                                backgroundColor: colors.buttonColor,
+                                textColor: Colors.white,
+                                gravity: ToastGravity.BOTTOM,
+                              ),
+                            );
+                        await profilePro
+                            .getvendorProfileData()
+                            .then((value) => Navigator.pop(context));
                       }
-                      return null;
                     },
-                  ),
-                  const Divider(color: Colors.transparent),
-                  TextFormField(
-                    controller: typeCtrl,
-                    textInputAction: TextInputAction.next,
-                    decoration: (const InputDecoration())
-                        .applyDefaults(Theme.of(context).inputDecorationTheme)
-                        .copyWith(labelText: "Business Type*"),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Enter businesss type";
-                      }
-                      return null;
-                    },
-                  ),
-                  const Divider(color: Colors.transparent),
-                  TextFormField(
-                    controller: nameCtrl,
-                    textInputAction: TextInputAction.next,
-                    decoration: (const InputDecoration())
-                        .applyDefaults(Theme.of(context).inputDecorationTheme)
-                        .copyWith(labelText: "Company or Business Name*"),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Enter Company or Business Name*";
-                      }
-                      return null;
-                    },
-                  ),
-                  const Divider(color: Colors.transparent),
-                  TextFormField(
-                    controller: gstinCtrl,
-                    inputFormatters: [RegexFormatter.regNo],
-                    textCapitalization: TextCapitalization.characters,
-                    maxLength: 15,
-                    textInputAction: TextInputAction.next,
-                    decoration: (const InputDecoration())
-                        .applyDefaults(Theme.of(context).inputDecorationTheme)
-                        .copyWith(labelText: "Enter GSTIN*", counterText: ""),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Please enter GSTIN";
-                      }
-                      if (value.length < 15) {
-                        return "Please enter valid GST";
-                      }
-                      return null;
-                    },
-                  ),
-                  const Divider(color: Colors.transparent),
-                  TextFormField(
-                    controller: regCtrl,
-                    textInputAction: TextInputAction.next,
-                    inputFormatters: [RegexFormatter.regNo],
-                    textCapitalization: TextCapitalization.characters,
-                    maxLength: 21,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Please enter Registration No.";
-                      }
-                      if (value.length < 21) {
-                        return "Please enter valid Reg No.";
-                      }
-                      return null;
-                    },
-                    decoration: (const InputDecoration())
-                        .applyDefaults(Theme.of(context).inputDecorationTheme)
-                        .copyWith(
-                            labelText:
-                                "Business Registration Number (if applicable)",
-                            counterText: ""),
-                  ),
-                  const Divider(color: Colors.transparent),
-                  TextFormField(
-                    controller: tinCtrl,
-                    textInputAction: TextInputAction.done,
-                    inputFormatters: [RegexFormatter.regNo],
-                    textCapitalization: TextCapitalization.characters,
-                    maxLength: 10,
-                    decoration: (const InputDecoration())
-                        .applyDefaults(Theme.of(context).inputDecorationTheme)
-                        .copyWith(
-                            labelText: "Permanent Account Number (PAN)*",
-                            counterText: ""),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Please enter Permanent Account Number";
-                      }
-                      if (value.length < 10) {
-                        return "Please enter Permanent Account Number";
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(
-                    height: height * .24,
-                  ),
-                  ElevatedButton(
-                      onPressed: () async {
-                        if (_formKey.currentState!.validate()) {
-                          await profilePro
-                              .updateBusinessDetail(
-                                bussiness_email_id: emailCtrl.text.toString(),
-                                bussiness_type: typeCtrl.text.toString(),
-                                company_name: nameCtrl.text.toString(),
-                                gst_in: gstinCtrl.text.toString().toUpperCase(),
-                                bussiness_registeration_number:
-                                    regCtrl.text.toString().toUpperCase(),
-                                tax_identification_number:
-                                    tinCtrl.text.toString().toUpperCase(),
-                              )
-                              .then(
-                                (value) => Fluttertoast.showToast(
-                                  msg: value['msg'],
-                                  backgroundColor: colors.buttonColor,
-                                  textColor: Colors.white,
-                                  gravity: ToastGravity.BOTTOM,
-                                ),
-                              );
-                          await profilePro
-                              .getvendorProfileData()
-                              .then((value) => Navigator.pop(context));
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                          fixedSize: Size(width * .9, 50)),
-                      child: Text(
-                        "SAVE",
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )),
-                ],
-              ),
+                    style: ElevatedButton.styleFrom(
+                        fixedSize: Size(width * .9, 50)),
+                    child: Text(
+                      "SAVE",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )),
+              ],
             ),
           ),
         ),
